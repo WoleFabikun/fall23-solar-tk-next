4ec4cad1342705697045a9d2f74aad8e94d916a4
Wole Fabikun <wole359@gmail.com>
2024-05-05 23:42:15 -0400

    update

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return <footer className='py-6'>
     <div className='flex flex-col items-center justify-between'>
       <div className=' text-muted-foreground'>
-        Copyright &copy; Oluwole Fabikun. All rights reserved
+        Copyright &copy; LASS. All rights reserved
       </div>
     </div>
   </footer>
